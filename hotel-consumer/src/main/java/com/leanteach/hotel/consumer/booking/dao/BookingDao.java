c3334136fa913edb4e20016c6a5b8ffd05536b1e
dmartinez <dmartinez@kometsales.com>
2022-01-02 15:06:40 -0500

    Add repository annotation to repository classes

--- hotel-consumer/src/main/java/com/leanteach/hotel/consumer/booking/dao/BookingDao.java
+++ hotel-consumer/src/main/java/com/leanteach/hotel/consumer/booking/dao/BookingDao.java
@@ -2,7 +2,9 @@ package com.leanteach.hotel.consumer.booking.dao;
 
 import com.leanteach.hotel.consumer.entity.Booking;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookingDao extends CrudRepository<Booking, Long> {
 
 
--- hotel/src/main/java/com/leantech/hotel/producer/booking/dao/BookingDao.java
+++ hotel/src/main/java/com/leantech/hotel/producer/booking/dao/BookingDao.java
@@ -2,7 +2,9 @@ package com.leantech.hotel.producer.booking.dao;
 
 import com.leantech.hotel.producer.entity.Booking;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookingDao extends CrudRepository<Booking, Long> {
 
 
